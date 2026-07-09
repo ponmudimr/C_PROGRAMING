11a6b1c6997d520078691c1d60ab46d697baa365
ponmudimr <mrponmudi747@gmail.com>
2026-07-09 21:48:48 +0530

    Add diamond pattern

--- triangle_pattern.c
+++ triangle_pattern.c
@@ -64,5 +64,26 @@ int main() {
         printf("\n");
     }
 
+    // Pattern 6: Diamond (pyramid + inverted pyramid)
+    printf("\nDiamond:\n");
+    for (int i = 1; i <= rows; i++) {
+        for (int s = 1; s <= rows - i; s++) {
+            printf(" ");
+        }
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = rows - 1; i >= 1; i--) {
+        for (int s = 1; s <= rows - i; s++) {
+            printf(" ");
+        }
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
     return 0;
 }
