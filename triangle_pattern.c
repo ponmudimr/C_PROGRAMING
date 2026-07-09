f01da37f9d59f3ef8520fdd498d4e003bd48f591
ponmudimr <mrponmudi747@gmail.com>
2026-07-09 21:44:30 +0530

    Add triangle pattern printing program

---
+++ triangle_pattern.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main() {
+    int rows;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    // Pattern 1: Right-angled triangle of stars
+    printf("\nRight-angled triangle:\n");
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    // Pattern 2: Pyramid (centered triangle)
+    printf("\nPyramid:\n");
+    for (int i = 1; i <= rows; i++) {
+        for (int s = 1; s <= rows - i; s++) {
+            printf(" ");
+        }
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // Pattern 3: Number triangle
+    printf("\nNumber triangle:\n");
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    // Pattern 4: Inverted triangle
+    printf("\nInverted triangle:\n");
+    for (int i = rows; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
